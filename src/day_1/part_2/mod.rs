1b88cbea65a79f40928d83fcf70b4e5a2af58d4f
Linus Nagel <linusnag@gmail.com>
2024-12-01 12:57:58 +0100

    day_1 part_2

--- src/day_1/part_2/mod.rs
+++ src/day_1/part_2/mod.rs
@@ -1,2 +1,19 @@
+use std::collections::HashMap;
+
+use super::part_1::parse;
+
 #[cfg(test)]
 mod test;
+
+pub fn solve(input: &str) -> u64 {
+    let input = parse(input);
+    let (lefts, rights): (Vec<i64>, Vec<i64>) = input.unzip();
+    let rights_occurances = rights.iter().fold(HashMap::new(), |mut acc, r| {
+        *acc.entry(*r).or_insert(0) += 1;
+        acc
+    });
+    lefts.iter().map(|l| {
+        let r = rights_occurances.get(l).unwrap_or(&0);
+        (l * r) as u64
+    }).sum()
+}
